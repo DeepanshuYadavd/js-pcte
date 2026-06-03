19f764e6bd821d7bb8bd28251f9c2e4c854f12b3
DeepanshuYadavd <deepanshu.yadav@sensationsolutions.in>
2026-06-03 17:11:38 +0530

    string and number

---
+++ number.js
@@ -0,0 +1,20 @@
+// let a = 5;
+// let b = 7;
+// console.log(typeof a);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+
+//  special numbers:
+// console.log(10 / 0);
+
+// console.log("abc" - 10);
+
+// console.log(typeof NaN);
+
+// console.log("5" + 5);
+// console.log("5a" - 5);
+
+
---
+++ string.js
@@ -0,0 +1,74 @@
+// //  string :
+
+// let car = "defender";
+
+// car = "defender";
+
+// car = `defender`;
+
+// let s = `black car`;
+
+// console.log(s);
+
+//  operations:
+// let car = "defender";
+
+// console.log(car.length);
+
+// console.log(car[3]);
+
+// console.log(car.indexOf("n"));
+
+//  split:
+// let s = "mango grapes banana";
+
+// console.log(s.split(" "));
+
+// let car = "defender";
+// console.log(car.toUpperCase());
+// console.log(car.toLowerCase());
+// console.log(car.includes("f"));
+// console.log(car.includes("x"));
+
+//  replace:
+
+// let name = "deepanshu";
+// console.log(name.replace("p", "v"));
+// let fruits = "mango mango mango";
+// console.log(fruits.replace("mango", "apple"));
+// console.log(fruits.replaceAll("mango", "apple"));
+
+//  slice :
+
+//  slice is used to access the subpart of the string
+// let p = "javascript";
+
+// console.log(p.slice(4, 6));
+
+// let a = "excellent";
+// console.log(a.slice(2, 6));
+
+// let n = "deepanshu";
+// console.log(n.slice(1));
+
+//  trim:
+// it is used to remove the end spaces from the string
+// let n = "    deepanshu";
+// console.log(n);
+
+// console.log(n.trim());
+
+// let n = " Hip Hop ";
+// console.log(n.length);
+// console.log(n.trim().length);
+
+//  concat:
+// it is used to add to or more string:
+
+// let str1 = "I Love ";
+// let str2 = "Western Dance";
+
+// console.log(str1, str2);
+// console.log(str1.concat(str2));
+
+// console.log(typeof str1);
