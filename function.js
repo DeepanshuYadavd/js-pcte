cbf0ff4ec1512521e84684f9c517d2db96aeea43
DeepanshuYadavd <deepanshu.yadav@sensationsolutions.in>
2026-06-06 12:57:53 +0530

    functions update

--- function.js
+++ function.js
@@ -3,13 +3,55 @@
 //  arrow function
 
 //  normal function:
-function operations(a, b, opr) {
-  if (opr === "sum") {
-    return `${opr}=${a + b}`;
-  }
-  if (opr === "mul") {
-    return `${opr}=${a * b}`;
+// function operations(a, b, opr) {
+//   if (opr === "sum") {
+//     return `${opr}=${a + b}`;
+//   }
+//   if (opr === "mul") {
+//     return `${opr}=${a * b}`;
+//   }
+// }
+// console.log(operations(63, 99, "sum"));
+// console.log(operations(45, 67, "mul"));
+
+//  traffic que:
+// function traffic_light(light) {
+//   if (light === "red") {
+//     return "stop!!!";
+//   }
+//   if (light === "red") {
+//     return "stop!!!";
+//   }
+//   if (light === "green") {
+//     return "go!!!";
+//   }
+//   if (light === "yellow") {
+//     return "wait!!!";
+//   }
+// }
+// console.log(traffic_light("yellow"));
+
+// function traffic_light(light) {
+//   switch (light) {
+//     case "red":
+//       return "stop";
+//       break;
+//     case "yellow":
+//       return "wait!!";
+//       break;
+//     case "green":
+//       return "go!!";
+//       break;
+//     default:
+//       return "invalid";
+//   }
+// }
+// console.log(traffic_light("red"));
+
+// function to print table :
+function table(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i}= ${num * i}`);
   }
 }
-console.log(operations(63, 99, "sum"));
-console.log(operations(45, 67, "mul"));
+table(13);
